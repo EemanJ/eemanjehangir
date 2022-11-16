dcdc0a4aa69fc97bc2a53b7aca52132baf4369e9
EemanJ <eemanjehangir@hotmail.com>
2022-11-17 00:41:19 +0500

    Editted component names in index1.js file to match file name.

--- src/pages/index1.js
+++ src/pages/index1.js
@@ -123,7 +123,7 @@ const links = [
   },
 ]
 
-const IndexPage = () => {
+const IndexPage1 = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
@@ -171,6 +171,6 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage
+export default IndexPage1
 
 export const Head = () => <title>Home Page</title>
